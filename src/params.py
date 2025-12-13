80b691ffcec7a4595c8d3279065899bff10a69f6
Pol Verdura <pverdura6@gmail.com>
2025-12-13 17:03:29 +0100

    Model storage saved

--- src/params.py
+++ src/params.py
@@ -1,3 +1,5 @@
-path = "."    # Relative path of the file
+path = "."          # Relative path of the file
 file = "data.csv"   # Name of the file
+model = ".model"    # Folder for storing the trained models
 
+path_model = path+"/"+model+"/"
--- src/train.py
+++ src/train.py
@@ -1,6 +1,8 @@
 import pandas as pd                                         # For managing data
 from sklearn.ensemble import HistGradientBoostingClassifier # For training and managing the model
 from skl2onnx import to_onnx                                # For storing the data
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType
 
 import model
 import params
@@ -14,19 +16,21 @@ def __main__():
     for i in df:
         ids = i
 
-    print(df)
     y = df[ids]
     X = df.drop(columns=[ids])
     
     # We train the model with the data
     reg, cols, loss = model.train(X, y)
-'''
-    # We store the trained model
+
+    # We store the trained model in <path_model+name>
     name = params.file[:-4]
-    print(name)
-    onx = to_onnx(reg, X[:1])
-    with open(path+"/.models/"+name, "wb") as f:
-        f.write(onx.SerializeToString())'''
+
+    initial_type = [('input', FloatTensorType([None, X.shape[1]]))]
+
+    onx = convert_sklearn(reg, initial_types=initial_type)
+
+    with open(params.path_model+name+".onnx", "wb") as f:
+        f.write(onx.SerializeToString())
 
 
 
