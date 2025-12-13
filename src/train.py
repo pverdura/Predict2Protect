bf9fa2ee386159564e08b3e7078a7029d3c948ca
maxerrmax <maxvillalbapons@gmail.com>
2025-12-13 20:07:07 +0100

    Generated training output

--- src/train.py
+++ src/train.py
@@ -20,6 +20,16 @@ def __main__():
     # We train the model with the data
     reg, cols, loss = model.train(X, y)
 
+    X_encoded = model.preprocess_one_hot(X, columns_trained=cols)
+    y_prob = model.predict_probability(reg, X_encoded)
+
+    df_pred = X.copy()  # inclou les columnes originals
+    df_pred['pred_prob_class_1'] = y_prob
+    df_pred['pred_class'] = (y_prob >= 0.5).astype(int)
+
+    df_pred.to_csv("prediccions.csv", index=False)
+    print("CSV de prediccions generat a: prediccions.csv")
+
     # We store the trained model in <path_model+name>
     filename = params.path_model+params.train_file[:-4]+".sav"
 
