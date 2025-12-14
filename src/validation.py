481aa4717419d254c0adba129335783a72d97f67
maxerrmax <maxvillalbapons@gmail.com>
2025-12-14 11:51:00 +0100

    Model prediction added

--- src/validation.py
+++ src/validation.py
@@ -1,19 +1,26 @@
-import pandas as pd                                         # For managing data
-from sklearn.ensemble import HistGradientBoostingClassifier # For training and managing the model
-import pickle                                               # For storing the data
+import pandas as pd
+from sklearn.ensemble import HistGradientBoostingClassifier
+import pickle
 
 import params
 
 
 def __main__():
-    # We read the file with the data to validate
-    df = pd.read_csv(params.path+"/"+params.valid_file, sep=",")
+    # Read validation data
+    df = pd.read_csv(params.path + "/" + params.valid_file, sep=",")
 
-    # We load the pre-trained model 
-    filename = params.path_model+params.valid_file[:-4]+".sav"
-    
+    # Load trained model
+    filename = params.path_model + params.valid_file[:-4] + ".sav"
     model = pickle.load(open(filename, 'rb'))
 
+    # Predict probability of the positive class
+    y_proba = model.predict_proba(df)[:, 1]
+
+    # Save probabilities to CSV
+    output_file = params.path + "/predictions_" + params.valid_file
+    pd.DataFrame({"proba_positive_class": y_proba}).to_csv(output_file, index=False)
 
 
 __main__()
+
+
