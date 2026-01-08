beabe0d6366d655cab54dcdad5e8cf8189bcba80
Pol Verdura <pverdura6@gmail.com>
2026-01-08 07:34:05 +0100

    CSV dataset merge soved

--- src/params.py
+++ src/params.py
@@ -3,5 +3,7 @@ train_allele  = "data.csv" # Name of the file used to train the model
 train_patient = "patients.csv"
 valid_file    = "data.csv" # Name of the file used to validate the model
 model_dir     = ".model"   # Folder for storing the trained models
+csv_sep       = ","
 
 path_model = path+"/"+model_dir+"/"
+
--- src/train.py
+++ src/train.py
@@ -1,32 +1,45 @@
+# src/train.py - Trains and stores the medication model
+
 import pandas as pd                                         # For managing data
 from sklearn.ensemble import HistGradientBoostingClassifier # For training and managing the model
-import pickle                                               #  For storing the data
+import pickle                                               # For storing the data
 
-import model
-import params
+import model    # Trains the model
+import params   # General metadata
 
-def join_df(df_allele, df_person):
+
+### INTERSECTS BOTH DATASETS ###
+def join_df(df_allele, df_patient):
     # We change the data frame index to ID
-    df_allele = df_allele.rename(columns={df_allele.columns[0]: "ID"})
-    df_allele = df_allele.set_index('ID')
-    df_person = df_person.set_index('ID')
+    df_allele  = df_allele.rename(columns={df_allele.columns[0]: "ID"})
+    df_allele  = df_allele.set_index('ID')
 
-    df_allele = pd.merge(df_allele, df_person, how='left', on="ID")
+    # We are interested in 'RM profunda' (treatment) and
+    # TODO: duration
+    df_patient = df_patient.set_index('ID')
+    df_patient = df_patient.loc[:, ['RM profunda (BCR/ABL < 001%)']]
+    
+    # We intersect the databases
+    df_allele = pd.merge(df_allele, df_patient, how='left', on="ID")
 
+    # We divide the dataset between input and output
     y = df_allele[["RM profunda (BCR/ABL < 001%)"]]
     X = df_allele.drop(columns=["RM profunda (BCR/ABL < 001%)"])
 
     return X, y
 
 
+### MAIN FUNCTION ###
 def __main__():
     # Read and process the data
-    df_allele = pd.read_csv(params.path+"/"+params.train_allele, sep=";")
-    df_person = pd.read_csv(params.path+"/"+params.train_patient, sep=";")
+    df_allele = pd.read_csv(params.path+"/"+params.train_allele, sep=params.csv_sep)
+    df_patient = pd.read_csv(params.path+"/"+params.train_patient, sep=params.csv_sep)
 
-    X, y = join_df(df_allele, df_person)
+    X, y = join_df(df_allele, df_patient)
+
 
 if 0:
+    # TODO:
     # We train the model with the data
     reg, cols, loss = model.train(X, y)
 
